f91aed094c61289a7c088c6f6ed376afbd7941f6
yasermoamd <yasermoamd@hotmail.com>
2024-06-30 13:55:41 -0700

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+#!/usr/bin/pp
+# install especific version of flask (2.1.0)
+package {'flask':
+	ensure => '2.1.0',
+	provider => 'pip3'
 }
